669f70ac66017615659afcd7d368ed80855c9478
Mattias Schertell <Mattias Schertell>
2017-01-17 14:27:17 +0100

    vagrant configs for provisioning

---
+++ vagrant/setup.sh
@@ -0,0 +1,16 @@
+# Change these to suit your needs...
+DBHOST=localhost
+DBNAME=dbname
+DBUSER=dbuser
+DBPASSWD=test123
+DB_ROOT_PASS=password123
+
+apt-get -y install curl build-essential python-software-properties git python3
+
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_ROOT_PASS"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASS"
+
+apt-get -y install mysql-server
+
+# mysql -uroot -p$DB_ROOT_PASS -e "CREATE DATABASE $DBNAME"
+mysql -uroot -p$DB_ROOT_PASS -e "grant all privileges on $DBNAME.* to '$DBUSER'@'$DBHOST' identified by '$DBPASSWD'" 
