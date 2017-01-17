231a1c4294cf25f41243d9c9bb17f6ed684ad93c
Mattias Schertell <schertell@gmail.com>
2017-01-17 14:57:31 +0100

    added pip3 to provision

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -5,7 +5,7 @@ DBUSER=dbuser
 DBPASSWD=test123
 DB_ROOT_PASS=password123
 
-apt-get -y install curl build-essential python-software-properties git python3
+apt-get -y install curl build-essential python-software-properties git python3 python3-pip
 
 debconf-set-selections <<< "mysql-server mysql-server/root_password password $DB_ROOT_PASS"
 debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DB_ROOT_PASS"
